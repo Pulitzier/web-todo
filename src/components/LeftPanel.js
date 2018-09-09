56a62deb227b9905b1617bff37a288da7fb55da4
ybaravy <ybaravy@klika-tech.com>
2018-09-09 23:29:20 +0300

    Close Search when click on 'New List'

--- src/components/LeftPanel.js
+++ src/components/LeftPanel.js
@@ -34,114 +34,114 @@ export default class LeftPanel extends Component {
     const { store } = this.context;
     const state = store.getState();
     const { app: { todos, tasks } } = state;
-
     const addNewList = () => {
-    let newListTitle = 'Untitled Task';
-    todos.map(item => {
-      if (item.title.indexOf('Untitled Task') !== -1) {
-        if (isNaN(parseInt(item.title.replace( /[^\d.]/g, '' )))) {
-          newListTitle = 'Untitled Task ' + 1;
-        } else {
-          let index = parseInt(item.title.replace( /[^\d.]/g, '' )) + 1;
-          newListTitle = 'Untitled Task '+index;
-        };
-      }
-    });
-    store.dispatch(setNewListTitle(newListTitle));
-    store.dispatch(activateNewList(true));
-  };
+      store.dispatch(openSearchPanel(false));
+      let newListTitle = 'Untitled Task';
+      todos.map(item => {
+        if (item.title.indexOf('Untitled Task') !== -1) {
+          if (isNaN(parseInt(item.title.replace( /[^\d.]/g, '' )))) {
+            newListTitle = 'Untitled Task ' + 1;
+          } else {
+            let index = parseInt(item.title.replace( /[^\d.]/g, '' )) + 1;
+            newListTitle = 'Untitled Task '+index;
+          };
+        }
+      });
+      store.dispatch(setNewListTitle(newListTitle));
+      store.dispatch(activateNewList(true));
+    };
 
-  const pushNewListToState = () => {
-    store.dispatch(activateNewList(false));
-    store.dispatch(addNewTodoList(state.newListTitle))
-    store.dispatch(setNewListTitle('Untitled Task'));
-  };
+    const pushNewListToState = () => {
+      store.dispatch(activateNewList(false));
+      store.dispatch(addNewTodoList(state.newListTitle))
+      store.dispatch(setNewListTitle('Untitled Task'));
+    };
 
-  const typeNewListTitle = (e) => {
-    let newListTitle = e.target.value;
-    store.dispatch(setNewListTitle(newListTitle));
-  };
+    const typeNewListTitle = (e) => {
+      let newListTitle = e.target.value;
+      store.dispatch(setNewListTitle(newListTitle));
+    };
 
-  const chooseListItem = (todoId) => {
-    store.dispatch(openSearchPanel(false));
-    store.dispatch(activateTask(false));
-    store.dispatch(chooseList(todoId))
-  };
+    const chooseListItem = (todoId) => {
+      store.dispatch(openSearchPanel(false));
+      store.dispatch(activateTask(false));
+      store.dispatch(chooseList(todoId))
+    };
 
-  return (
-    <Panel className="col-md-4 leftPanel">
-      <UserSettings />
-      <List className="nav flex-column my-todo-list">
-        {todos.map(todo => {
-          if (todo.todoListId < 3) {
-            return (
-              <li className={"nav-item "+ (todo.active ? 'active' : '')} key={todo.todoListId}>
-                <a
-                  className={"nav-link " + (todo.active ? 'active' : '')}
-                  onClick={() => chooseListItem(todo.todoListId)}
+    return (
+      <Panel className="col-md-4 leftPanel">
+        <UserSettings />
+        <List className="nav flex-column my-todo-list">
+          {todos.map(todo => {
+            if (todo.todoListId < 3) {
+              return (
+                <li className={"nav-item "+ (todo.active ? 'active' : '')} key={todo.todoListId}>
+                  <a
+                    className={"nav-link " + (todo.active ? 'active' : '')}
+                    onClick={() => chooseListItem(todo.todoListId)}
+                  >
+                    <img
+                      src={(() => {
+                        if(todo.title === 'My Day') {
+                          return './assets/sun.svg'
+                        }
+                        if(todo.title === 'Important') {
+                          return './assets/star.svg'
+                        }
+                        if(todo.title === 'To-Do') {
+                          return './assets/home.svg'
+                        }
+                        return '';
+                      })()}
+                      alt='Categories Icon'
+                    />
+                    <p>{todo.title}</p>
+                    <span>{(() => getTasksForTodo(tasks, todo).length ? getTasksForTodo(tasks, todo).length : '')()}</span>
+                  </a>
+                </li>
+              )
+            }
+          })}
+        </List>
+        <hr />
+        <List className="nav flex-column todo-list">
+          {todos.map(todo => {
+            if (todo.todoListId >= 3) {
+              return (
+                <li
+                  className={"nav-item "+ (todo.active ? 'active' : '')}
+                  key={todo.todoListId}
                 >
-                  <img
-                    src={(() => {
-                      if(todo.title === 'My Day') {
-                        return './assets/sun.svg'
-                      }
-                      if(todo.title === 'Important') {
-                        return './assets/star.svg'
-                      }
-                      if(todo.title === 'To-Do') {
-                        return './assets/home.svg'
-                      }
-                      return '';
-                    })()}
-                    alt='Categories Icon'
-                  />
-                  <p>{todo.title}</p>
-                  <span>{(() => getTasksForTodo(tasks, todo).length ? getTasksForTodo(tasks, todo).length : '')()}</span>
-                </a>
-              </li>
-            )
-          }
-        })}
-      </List>
-      <hr />
-      <List className="nav flex-column todo-list">
-        {todos.map(todo => {
-          if (todo.todoListId >= 3) {
-            return (
-              <li
-                className={"nav-item "+ (todo.active ? 'active' : '')}
-                key={todo.todoListId}
-              >
-                <a
-                  className="nav-link"
-                  onClick={() => chooseListItem(todo.todoListId)}
-                >
-                  {todo.title}
-                  <span></span>
-                </a>
-              </li>
-            )
-          }
-        })}
-      </List>
-      <div className="add-new-list" onBlur={() => pushNewListToState()}>
-        <input
-          type="text"
-          ref={node => this.newListTitleInput = node}
-          className={"add-new-list-label "+(state.activateNewList ? 'active' : 'inactive')}
-          onChange={typeNewListTitle}
-          value={state.newListTitle}
-          autoFocus={state.activateNewList}
-        />
-        <a
-          className="add-new-list-link"
-          onClick={() => addNewList()}
-        >
-          <span>+</span> New List
-        </a>
-      </div>
-    </Panel>
-  );
+                  <a
+                    className="nav-link"
+                    onClick={() => chooseListItem(todo.todoListId)}
+                  >
+                    {todo.title}
+                    <span></span>
+                  </a>
+                </li>
+              )
+            }
+          })}
+        </List>
+        <div className="add-new-list" onBlur={() => pushNewListToState()}>
+          <input
+            type="text"
+            ref={node => this.newListTitleInput = node}
+            className={"add-new-list-label "+(state.activateNewList ? 'active' : 'inactive')}
+            onChange={typeNewListTitle}
+            value={state.newListTitle}
+            autoFocus={state.activateNewList}
+          />
+          <a
+            className="add-new-list-link"
+            onClick={() => addNewList()}
+          >
+            <span>+</span> New List
+          </a>
+        </div>
+      </Panel>
+    );
   }
 };
 
