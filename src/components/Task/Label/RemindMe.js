9b6c70b15f679c3bcbe45293b082a032ef6daced
ybaravy <ybaravy@klika-tech.com>
2019-03-24 13:45:58 +0300

    Minor refactoring of RemindMe Sub Label

--- src/components/Task/Label/RemindMe.js
+++ src/components/Task/Label/RemindMe.js
@@ -1,11 +1,10 @@
 import Template from './Template';
 import { getStringDate } from '../../../helpers';
 
-export default class RemindSubLabel extends Template {
+export default class RemindMe extends Template {
   constructor(task) {
     super();
     this.shouldBeRendered = this.shouldBeRendered.bind(this);
-    this.generateLabelData = this.generateLabelData.bind(this);
     this.taskRemindDate = task.remindDate;
     this.setIconSrc("far fa-clock");
     this.shouldBeRendered() && this.setText(getStringDate(this.taskRemindDate));
